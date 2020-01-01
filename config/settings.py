4a230265a6dd4c017d239c8628637729a2ebdb21
startFromBottom <uhh0701@gmail.com>
2020-01-01 17:14:14 +0900

    convert fbv to cbv

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,10 @@ MEDIA_URL = "/media/"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
